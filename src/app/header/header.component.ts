37a7cf7d8343c2274092c7215738103c48afc1a6
Piyush Patel <piyushpatel2005@gmail.com>
2017-10-07 00:08:05 +0530

    Template driven form validation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MdDialog, MdDialogRef } from '@angular/material';
+import { MdDialog} from '@angular/material';
 import { LoginComponent } from '../login/login.component';
 
 @Component({
